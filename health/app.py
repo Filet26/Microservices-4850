6dd18b8d80101525e63b7a542e266cfc49651bc9
Desmond Ho <desmondh268@gmail.com>
2023-11-21 10:39:58 -0800

    change health base pth

--- health/app.py
+++ health/app.py
@@ -110,7 +110,9 @@ def get_heathcheck():
 
 # app config
 app = connexion.FlaskApp(__name__, specification_dir="./")
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api(
+    "openapi.yaml", base_path="/health", strict_validation=True, validate_responses=True
+)
 CORS(app.app)
 app.app.config["CORS_HEADERS"] = "Content-Type"
 
